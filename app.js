7cf27f152ac02c34c487d2f451902894d2bdb012
MariusTanase <pulsemusic8@hotmail.com>
2022-05-18 19:29:36 +0100

    Basic functions and Interger work, float WIP

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //Global variable to avoid repetition
 
 let buttons = document.querySelectorAll('.calculator__button');
-
 //Event Listener for all the buttons from HTML
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function (e) {
@@ -15,27 +14,61 @@ for (let i = 0; i < buttons.length; i++) {
 };
 
 
-
 //Get the result from the display
 function getResult() {
   let input = document.querySelector('.calculator__display-text');
   //making eval a save option to use in our function
   let operator = input.value.match(/[\D]/g);
-  console.log(operator);
-  let numbers = input.value.split(""); //in works
+
+  //regex numbers to be split together with divident .
+  let numbers = input.value.match(/([\d.]+)+/g);
+    // let numbers = input.value.split(/([\d]+)+/g);
   console.log(numbers);
-  // input.value = eval(input.value);
-  // input.textContent = input.value;
-  // Looking for alternatives to eval
+
+  // filter empty strings and undevined from numbers array
+  // numbers = numbers.filter(function (n) {
+  //   return n !== '' && n !== undefined;
+  // });
+  // return operator and numbers array to calculate them based on operator
+  calculate(operator, numbers[0], numbers[2]);
 };
+
+//calculate the result
+function calculate(operator, num1, num2) {
+  let input = document.querySelector('.calculator__display-text');
+  let result = 0;
+  // if operator is empty return the numbers array
+  if (operator === null) {
+    return input.value;
+  } else {// if operator is not empty calculate the result
+    switch (operator[0]) {
+      case '+':
+        result = parseFloat(num1) + parseFloat(num2);
+        break;
+      case '-':
+        result = parseFloat(num1) - parseFloat(num2);
+        break;
+      case '*':
+        result = parseFloat(num1) * parseFloat(num2);
+        break;
+      case '/':
+        result = parseFloat(num1) / parseFloat(num2);
+        break;
+      default:
+        result = 'error';
+    }
+    input.value = result;
+    input.textContent = result;
+  };
+};
+
 
 //Send numbers and operators from buttons to display
 function sendToDisplay(value) {
-let input = document.querySelector('.calculator__display-text');
+  let input = document.querySelector('.calculator__display-text');
   if (input.value === undefined) {
-    input.value = 0;
+    input.value = '';
   }
   input.value += value;
   input.textContent += value;
 }
-
