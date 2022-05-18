a278a645076c741ae390f88397468830fab84ea2
Marius Tanase <pulsemusic8@gmail.com>
2022-05-18 18:03:31 +0100

    Function to get results from screen

---
+++ app.js
@@ -0,0 +1,12 @@
+//Get the result from the display
+function getResult() {
+  var input = document.querySelector('.calculator__display-text');
+  //making eval a save option to use in our function
+  let operator = input.value.match(/[\D]/g);
+  console.log(operator);
+  let numbers = input.value.match(/[\d]/g);
+  console.log(numbers);
+  // input.value = eval(input.value);
+  // input.textContent = input.value;
+}
+
