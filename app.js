4ebb79f4e311ac84d02908f5315b1cbf6d68aeb9
MariusTanase <pulsemusic8@hotmail.com>
2022-05-18 18:31:24 +0100

    rendering values to screen, results WIP

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
+//Global variable to avoid repetition
+
+let buttons = document.querySelectorAll('.calculator__button');
+
+//Event Listener for all the buttons from HTML
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function (e) {
+    let clicked = e.target.value;
+    if (clicked === '=') {
+      getResult();
+    } else {
+      sendToDisplay(clicked);
+    }
+  });
+};
+
+
+
 //Get the result from the display
 function getResult() {
   let input = document.querySelector('.calculator__display-text');
   //making eval a save option to use in our function
   let operator = input.value.match(/[\D]/g);
   console.log(operator);
-  let numbers = input.value.match(/[\d]/g);
+  let numbers = input.value.split(""); //in works
   console.log(numbers);
   // input.value = eval(input.value);
   // input.textContent = input.value;
   // Looking for alternatives to eval
 };
 
-//Event Listener for all the buttons from HTML
-let buttons = document.querySelectorAll('.calculator__button');
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function (e) {
-    let clicked = e.target.value;
-    if (clicked === '=') {
-      getResult();
-    }
-  });
-};
+//Send numbers and operators from buttons to display
+function sendToDisplay(value) {
+let input = document.querySelector('.calculator__display-text');
+  if (input.value === undefined) {
+    input.value = 0;
+  }
+  input.value += value;
+  input.textContent += value;
+}
+
